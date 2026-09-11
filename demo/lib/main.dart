9faa73ab5ed55874612e1e883a2d92d6a853b52a
Ariel-Amoguis <arielamoguis7@gmail.com>
2026-09-11 04:36:52 +0000

    first flutter activity

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -13,12 +13,55 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text(
-            'Hello World', style: TextStyle(
-              fontSize: 50,
-              color: Colors.purple
-              
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children:<Widget> [
+              Icon(
+                Icons.account_circle,
+                color: Colors.deepPurple,
+                size: 100.0
+              ),
+              Text('Rodney C. Gonzales', 
+              style: TextStyle(fontSize: 35),
+              ),
+              Text("Master's in Information Technology"),
+              SizedBox(height: 100),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                Icons.home,
+                color: Colors.deepPurple,
+                size: 20.0
+              ),
+              SizedBox(width: 50),
+              Icon(
+                Icons.menu_book,
+                color: Colors.deepPurple,
+                size: 20.0
+              ),
+              SizedBox(width: 50),
+              Icon(
+                Icons.person,
+                color: Colors.deepPurple,
+                size: 20.0
+              ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                Text("Home"),
+                SizedBox(width: 25),
+                Text("Course"),
+                SizedBox(width: 25),
+                Text("Profile"),
+                ],
+              ),
+              SizedBox(height: 300),
+              Text("Ang Pogi mo Rodney!"),
+            ],
           ),
         ),
       ),
